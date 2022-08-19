10b82c944ae35be5f9256cc03d17ab4147743127
sonali <sonalipatle8@gmail.com>
2022-08-19 16:01:58 +0530

    testing

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ threads_count_min = Integer(ENV['MIN_THREADS'] || 5)
 threads_count_max = Integer(ENV['MAX_THREADS'] || 30)
 threads threads_count_min, threads_count_max
 
-environment ENV.fetch('RAILS_ENV', 'development')
+environment ENV.fetch('RAILS_ENV', 'production')
 
 preload_app!
 
